49663c283fbed390b01f211afb5f937273ec6345
Julian Hesberg <112935968+JulianHesberg@users.noreply.github.com>
2025-05-05 10:04:43 +0200

    create models

---
+++ DatabasesComplusory/Models/Write/Review.cs
@@ -0,0 +1,10 @@
+namespace DatabasesComplusory.Models;
+
+public class Review
+{
+    public int ReviewId { get; set; }
+    public int UserId { get; set; }
+    public int SellerId { get; set; }
+    public int Rating { get; set; }
+    public string Comment { get; set; }
+}
